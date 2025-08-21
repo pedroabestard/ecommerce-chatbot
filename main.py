4b2311577e15de60bfe10ff2db930a6e2bf7c69d
Pedro Alejandro Bestard Hernández <pedroabestard@gmail.com>
2025-08-21 15:17:27 -0400

    patched pysqlite3 on main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import sys
+
+try:
+    import pysqlite3
+    sys.modules["sqlite3"] = pysqlite3
+except ImportError:
+    pass
+
 import streamlit as st
 from router import router
 from faq import ingest_faq_data, faq_chain
